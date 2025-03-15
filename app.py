66dc8218975fb404d383d92932fecc7ea7191b9e
John <s6604062630129@email.kmutnb.ac.th>
2025-03-15 15:14:23 +0700

    .jpg add

--- app.py
+++ app.py
@@ -95,13 +95,13 @@ elif menu == "🖼️ จำแนกประเภทสินค้าจา�
     st.write("สิ่งที่train มีสินค้าสามชนิดประกอบด้วย")
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.image("/assets/Smartphone", caption="รูปที่ 1", use_container_width=True)
+        st.image("/assets/Smartphone.jpg", caption="รูปที่ 1", use_container_width=True)
 
     with col2:
-        st.image("/assets/Laptop", caption="รูปที่ 2", use_container_width=True)
+        st.image("/assets/Laptop.jpg", caption="รูปที่ 2", use_container_width=True)
 
     with col3:
-        st.image("/assets/Tablet", caption="รูปที่ 3", use_container_width=True)
+        st.image("/assets/Tablet.jpg", caption="รูปที่ 3", use_container_width=True)
 
     uploaded_file = st.file_uploader("เลือกไฟล์ภาพ", type=["jpg", "png", "jpeg"])
     if uploaded_file is not None:
